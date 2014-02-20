1893d18ef6587087fe6ab1f32920760f00f69df8
grorem <gmrorem@gmail.com>
2014-02-19 22:24:36 -0700

    thingerses

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 70
+          scrollTop: target.offset().top - 100
         }, 500);
         return false;
       }
